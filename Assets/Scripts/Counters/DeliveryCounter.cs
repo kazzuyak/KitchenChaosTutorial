56c64625c5c5fca90c06629f248bacd1577b00d5
kazzuyak <kazzuyak@gmail.com>
2023-07-11 06:51:14 -0300

    implement DeliveryCounter using shader for the moving arrow

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,9 @@
+public class DeliveryCounter : BaseCounter {
+  public override void Interact(Player player) {
+    if (player.HasKitchenObject()) {
+      if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+        player.GetKitchenObject().DestroySelf();
+      }
+    }
+  }
+}
